eb03e916fdd49b5cbaeffa284281ddcd1d5d784a
Pritirekha23 <learnpriti23@gmail.com>
2022-08-20 22:30:54 -0700

    converting list into tuple and set

---
+++ 121_convert list into tuple.py
@@ -0,0 +1,12 @@
+# converting list into tuple
+  # By using tuply() function we can convert a list into tuple. But we cant change the list after turning it into tuple,because it becomes immutable.
+print('--ex--')
+days=['sun','mon','tues','wed','thu','fri','sat']
+t=tuple(days)
+print(t)
+
+
+# list into set
+days=['sun','mon','tues','wed','thu','fri','sat']
+t=set(days)
+print(t)
