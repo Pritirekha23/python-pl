efbb7abf64aa08c310958866f0a970980cb3495c
Pritirekha23 <learnpriti23@gmail.com>
2022-09-22 22:11:07 -0700

    program

---
+++ 149-pr3.py
@@ -0,0 +1,9 @@
+# Write a program to print the following string in a specific formatz
+ # sampling String: "Twinle, twinkle, little star,
+ #                            How I wonder what u are!
+ #                                     Up above the world so high,
+ #                                                Like a diamond in the sky.
+ #                      Twinle, twinkle, little star,
+ #                                          How I wonder what u are"
+
+print("Twinle, twinkle, little star,\n\t  How I wonder what u are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\nTwinle, twinkle, little star,\n\t How I wonder what u are")
